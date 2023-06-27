2d7ca17315a213ee9ea0d0d2362f766a8d513d54
Silvano Carnevale <130051040+SilvaC91@users.noreply.github.com>
2023-06-27 22:09:34 +0200

    Update main.sql

--- _10_Foreign_keys__INNER_JOIN/main.sql
+++ _10_Foreign_keys__INNER_JOIN/main.sql
@@ -5,3 +5,11 @@ where i.NAME = "garlic";
 select count(distinct m.NAME)  from meal m  
 inner join ingredient i on I.MEAL_ID = M.MEAL_ID 
 where  i.VEGETARIAN;
+
+#select   m.NAME  from meal m  
+#inner join ingredient i on I.MEAL_ID = M.MEAL_ID 
+#where  (i.GLUTEN_FREE and i.VEGAN) and 
+#group by m.NAME;
+
+select COUNT(I.MEAL_ID) / COUNT(distinct M.MEAL_ID) from MEAL M  
+inner join ingredient i on I.MEAL_ID = M.MEAL_ID;
