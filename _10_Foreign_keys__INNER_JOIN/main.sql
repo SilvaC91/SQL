be85e379313ac9f3b1a52b99404d0689b5d13fbc
Silvano Carnevale <130051040+SilvaC91@users.noreply.github.com>
2023-06-27 16:00:54 +0200

    Update main.sql

--- _10_Foreign_keys__INNER_JOIN/main.sql
+++ _10_Foreign_keys__INNER_JOIN/main.sql
@@ -2,7 +2,6 @@ select m.name, i.NAME  from meal m
 inner join ingredient i on I.MEAL_ID = M.MEAL_ID 
 where i.NAME = "garlic";
 
-select m.NAME ,count(m.MEAL_ID)  from meal m  
+select count(distinct m.NAME)  from meal m  
 inner join ingredient i on I.MEAL_ID = M.MEAL_ID 
-where  i.VEGETARIAN
-group by m.NAME  ; #DA RIVEDERE
+where  i.VEGETARIAN;
