de9b6302b09cb7de144b5b35845dc1854c74dd71
Silvano Carnevale <130051040+SilvaC91@users.noreply.github.com>
2023-06-28 00:41:15 +0200

    Update main.sql

--- _10_Foreign_keys__INNER_JOIN/main.sql
+++ _10_Foreign_keys__INNER_JOIN/main.sql
@@ -6,10 +6,13 @@ select count(distinct m.NAME)  from meal m
 inner join ingredient i on I.MEAL_ID = M.MEAL_ID 
 where  i.VEGETARIAN;
 
-#select   m.NAME  from meal m  
-#inner join ingredient i on I.MEAL_ID = M.MEAL_ID 
-#where  (i.GLUTEN_FREE and i.VEGAN) and 
-#group by m.NAME;
+select   M.NAME  from MEAL M  
+inner join ingredient I1 on I1.MEAL_ID = M.MEAL_ID 
+inner join ingredient I2 on I2.MEAL_ID = M.MEAL_ID 
+where  (I1.GLUTEN_FREE and I2.VEGAN)
+group by M.NAME;
 
 select COUNT(I.MEAL_ID) / COUNT(distinct M.MEAL_ID) from MEAL M  
 inner join ingredient i on I.MEAL_ID = M.MEAL_ID;
+
+
